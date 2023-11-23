b5eb877de7f02fdd330b011856118fb48017a50d
Mohammad Samadi <m.samadi1377@gmail.com>
2023-11-23 14:10:26 +0330

    feat: get fee from database

---
+++ CongestionTaxCalculator/Models/IVehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace CongestionTaxCalculator.Models;
+
+public interface IVehicle
+{
+    string GetVehicleType();
+}
+
+public class Motorbike : IVehicle
+{
+    public string GetVehicleType()
+    {
+        return "Motorbike";
+    }
+}
+
+public class Car : IVehicle
+{
+    public string GetVehicleType()
+    {
+        return "Car";
+    }
+}
---
+++ CongestionTaxCalculator/Services/CongestionTaxCalculator.cs
@@ -0,0 +1,121 @@
+using CongestionTaxCalculator.Contexts;
+using CongestionTaxCalculator.Models;
+
+namespace CongestionTaxCalculator.Services;
+
+public interface ICongestionTaxCalculatorService
+{
+    /**
+ * Calculate the total toll fee for one day
+ *
+ * @param vehicle - the vehicle
+ * @param dates   - date and time of all passes on one day
+ * @return - the total congestion tax for that day
+ */
+
+    int GetTax(IVehicle vehicle, DateTime[] dates);
+}
+
+public class CongestionTaxCalculatorService : ICongestionTaxCalculatorService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CongestionTaxCalculatorService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    /**
+ * Calculate the total toll fee for one day
+ *
+ * @param vehicle - the vehicle
+ * @param dates   - date and time of all passes on one day
+ * @return - the total congestion tax for that day
+ */
+
+    public int GetTax(IVehicle vehicle, DateTime[] dates)
+    {
+        var intervalStart = dates[0];
+        var totalFee = 0;
+        foreach (var date in dates)
+        {
+            var nextFee = GetTollFee(date, vehicle);
+            var tempFee = GetTollFee(intervalStart, vehicle);
+
+            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
+            var minutes = diffInMillies / 1000 / 60;
+
+            if (minutes <= 60)
+            {
+                if (totalFee > 0) totalFee -= tempFee;
+                if (nextFee >= tempFee) tempFee = nextFee;
+                totalFee += tempFee;
+            }
+            else
+            {
+                totalFee += nextFee;
+            }
+        }
+
+        if (totalFee > 60) totalFee = 60;
+        return totalFee;
+    }
+
+    private bool IsTollFreeVehicle(IVehicle vehicle)
+    {
+        if (vehicle == null) return false;
+        var vehicleType = vehicle.GetVehicleType();
+        return vehicleType.Equals(TollFreeVehicles.Motorcycle.ToString()) ||
+               vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
+               vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
+               vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
+               vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
+               vehicleType.Equals(TollFreeVehicles.Military.ToString());
+    }
+
+    public int GetTollFee(DateTime date, IVehicle vehicle)
+    {
+        if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
+
+        var hour = date.Hour;
+        var minute = date.Minute;
+        var time = hour.ToString("D2") + ":" + minute.ToString("D2");
+
+        var fee = _dbContext.Fees
+            .FirstOrDefault(hf => time.CompareTo(hf.StartHour) >= 0 &&
+                                  time.CompareTo(hf.EndHour) <= 0);
+
+        return fee?.Fee ?? 0;
+    }
+
+    private bool IsTollFreeDate(DateTime date)
+    {
+        var year = date.Year;
+        var month = date.Month;
+        var day = date.Day;
+
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+
+        if (year == 2013)
+            if ((month == 1 && day == 1) ||
+                (month == 3 && (day == 28 || day == 29)) ||
+                (month == 4 && (day == 1 || day == 30)) ||
+                (month == 5 && (day == 1 || day == 8 || day == 9)) ||
+                (month == 6 && (day == 5 || day == 6 || day == 21)) ||
+                month == 7 ||
+                (month == 11 && day == 1) ||
+                (month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)))
+                return true;
+        return false;
+    }
+
+    private enum TollFreeVehicles
+    {
+        Motorcycle = 0,
+        Tractor = 1,
+        Emergency = 2,
+        Diplomat = 3,
+        Foreign = 4,
+        Military = 5
+    }
+}
