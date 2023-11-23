3e23d4924638a51e1ff1b14a7c35e145c99843a8
Mohammad Samadi <m.samadi1377@gmail.com>
2023-11-23 13:57:16 +0330

    feat: add entity framework

---
+++ CongestionTaxCalculator/Migrations/20231123102557_Init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CongestionTaxCalculator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartHour = table.Column<string>(type: "TEXT", nullable: false),
+                    EndHour = table.Column<string>(type: "TEXT", nullable: false),
+                    Fee = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fees");
+        }
+    }
+}
--- CongestionTaxCalculator/Program.cs
+++ CongestionTaxCalculator/Program.cs
@@ -1,8 +1,16 @@
+using CongestionTaxCalculator.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Main"));
+});
 
 var app = builder.Build();
 
