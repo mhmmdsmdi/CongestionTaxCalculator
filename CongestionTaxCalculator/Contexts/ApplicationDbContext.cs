065df4a4b8215cba71c7befffed07814e2d8fbd0
Mohammad Samadi <m.samadi1377@gmail.com>
2023-11-23 13:53:57 +0330

    Add project files.

---
+++ CongestionTaxCalculator/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CongestionTaxCalculator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CongestionTaxCalculator.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<HourFee> Fees { get; set; }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ CongestionTaxCalculator/Models/Fee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CongestionTaxCalculator.Models;
+
+public class HourFee
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string StartHour { get; set; }
+
+    [Required]
+    public string EndHour { get; set; }
+
+    [Required]
+    public int Fee { get; set; }
+}
